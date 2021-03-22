9bd95a400109abe294af5b574f50d2aa4197b9e4
LiNengXi <757110254@qq.com>
2021-03-22 22:01:47 +0800

    优化SudokuCore.js代码，提高代码复用率。

--- SudokuCore.js
+++ SudokuCore.js
@@ -1,5 +1,8 @@
 const LEN = 9;
+//  用于比较行列九宫数组排序后的位置上数字是否准确
 const NOTREPEATROWS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//  组成九个数字元素数组的循环步骤数
+const CELLGROUPCOUNTER = 3;
 
 function loopHandler(cb) {
   for (let i = 0; i < LEN; i++) {
@@ -11,6 +14,7 @@ function loopHandler(cb) {
 
 class SudokuCore {
   constructor() {
+    this.__sudoku = [];
     this.levels = [
       {
         text: '简单',
@@ -32,57 +36,45 @@ class SudokuCore {
   }
 
   rowGenerate() {
-    let sudoku = [];
-
-    for (let i = 0; i < LEN; i++) {
-        let tmp = [];
-        
-        for (let j = 1; j <= LEN; j++) {
-            tmp.push('');
-        }
-
-        sudoku.push(tmp);
-    }
-
-    return sudoku;
+    this.__sudoku = Array.apply(null, { length: LEN }).map(() => {
+      return Array.apply(null, { length: LEN }).map(() => {
+        return '';
+      })
+    });
   }
 
-  /**
-   * 九宫格数组
-   */
-  inSquareds(i, j, sudoku) {
-    let x, z;
+  setSteps(idx) {
+    let p;
 
-    if (i < 3) {
-      x = 0;
-    } else if (i < 6) {
-      x = 3;
-    } else if (i < 9) {
-      x = 6;
+    if (idx < CELLGROUPCOUNTER) {
+      p = 0;
+    } else if (idx < CELLGROUPCOUNTER * 2) {
+      p = CELLGROUPCOUNTER;
+    } else if (idx < CELLGROUPCOUNTER * CELLGROUPCOUNTER) {
+      p = CELLGROUPCOUNTER * 2;
     }
 
-    if (j < 3) {
-      z = 0;
-    } else if (j < 6) {
-      z = 3;
-    } else if (j < 9) {
-      z = 6;
-    }
+    return p;
+  }
 
-    let counter = 3,
+  //  九宫格数组
+  inSquareds(i, j, sudoku = this.__sudoku) {
+    let x = this.setSteps(i),
+        z = this.setSteps(j),
         squareds = [];
 
-    for (let len = x + counter; x < len; x++ ) {
-      squareds.push(sudoku[x][z]);
-      squareds.push(sudoku[x][z + 1]);
-      squareds.push(sudoku[x][z + 2]);
+    for (let len = x + CELLGROUPCOUNTER; x < len; x++ ) {
+      for (let p = z; p < z + CELLGROUPCOUNTER; p++) {
+        squareds.push(sudoku[x][p]);
+      }
     }
 
     return squareds;
   }
 
-  inRows(i, j, sudoku) {
-    let row = [];
+  inRows(i) {
+    let sudoku = this.__sudoku,
+        row = [];
     
     for (let x = 0; x < LEN; x++) {
       row.push(sudoku[i][x]);
@@ -91,7 +83,7 @@ class SudokuCore {
     return row;
   }
 
-  inColumns(i, j, sudoku) {
+  inColumns(j, sudoku = this.__sudoku) {
     let column = [];
     
     for (let x = 0; x < LEN; x++) {
@@ -105,31 +97,30 @@ class SudokuCore {
     return Math.floor(Math.random() * LEN + 1);
   }
 
-  checkNumber(rand, i, j, sudoku) {
-    if (this.inRows(i, j, sudoku).indexOf(rand) === -1 &&
-        this.inColumns(i, j, sudoku).indexOf(rand) === -1 &&
-        this.inSquareds(i, j, sudoku).indexOf(rand) === -1) {
+  checkNumber(rand, i, j) {
+    if (this.inRows(i).indexOf(rand) === -1 &&
+        this.inColumns(j).indexOf(rand) === -1 &&
+        this.inSquareds(i, j).indexOf(rand) === -1) {
       
       return rand;
     }
 
-    return this.checkNumber(this.getRandomNumber(), i, j, sudoku);
+    return this.checkNumber(this.getRandomNumber(), i, j);
   }
 
   initializeSudoku() {
-    let sudoku = this.rowGenerate();
-    return this.renderSudoku(sudoku);
+    this.rowGenerate();
+    return this.renderSudoku();
   }
 
-  /**
-   * 因不会算法，通过try catch取巧完成数独的初始化生成
-   */
-  renderSudoku(sudoku) {
-    let _self = this;
+  //  因不会算法，通过try catch取巧完成数独的初始化生成
+  renderSudoku() {
+    let sudoku = this.__sudoku,
+        _self = this;
 
     try {
         loopHandler(function (i, j) {
-            let num = _self.checkNumber(_self.getRandomNumber(), i, j, sudoku);
+            let num = _self.checkNumber(_self.getRandomNumber(), i, j);
 
             sudoku[i][j] = num;
         });
@@ -140,9 +131,7 @@ class SudokuCore {
     }
   }
 
-  /**
-   * 此函数用来给数独随机留白以供玩家填空。
-   */
+  //  此函数用来给数独随机留白以供玩家填空。
   createBlankCell(sudoku, difficulty) {
     loopHandler(function (i, j) {
       if (Math.random() > difficulty) {
@@ -153,60 +142,34 @@ class SudokuCore {
     return sudoku;
   }
 
-  /**
-   * 此函数用来给玩家的结果检测是否完成。
-   */
+  checkSudokuRows(arr) {
+    return arr.every((ele, index) => ele === NOTREPEATROWS[index]);
+  }
+
+  //  此函数用来给玩家的结果检测是否完成。
   checkSudoku(sudoku) {
     for (let i = 0; i < LEN; i++) {
-      let rows = sudoku[i].slice();
-      rows.sort();
+      let rows = sudoku[i].slice().sort();
   
-      for (let j = 0; j < LEN; j++) {
-        if (rows[j] !== NOTREPEATROWS[j]) { return false; }
-      }
+      let isRowsTrue = this.checkSudokuRows(rows);
+      if (!isRowsTrue) { return isRowsTrue; }
+
+      let columns = this.inColumns(i, sudoku).sort();
+  
+      let isColumnsTrue = this.checkSudokuRows(columns);
+      if (!isColumnsTrue) { return isColumnsTrue; }
     }
-  
-    for (let i = 0; i < LEN; i++) {
-      let columns = [];
-      for (let j = 0; j < LEN; j++) {
-        columns.push(sudoku[j][i]);
-      }
-      columns.sort();
-  
-      for (let x = 0; x < LEN; x++) {
-        if (columns[x] !== NOTREPEATROWS[x]) { return false; }
-      }
-    }
-  
-    let counter = 3,
-        squareds = [];
-  
-    for (let i = 0; i < LEN; i += counter) {
-        for (let j = 0; j < LEN; j += counter) {
-            let tmp = [];
-  
-            for (let x = i, len = i + counter; x < len; x++) {
-                for (let z = j, len = j + counter; z < len; z++) {
-                    tmp.push(sudoku[x][z]);
-                }
-            }
-  
-            squareds.push(tmp);
-        }
-    }
-  
-    for (let i = 0; i < LEN; i++) {
-      let nineGrids = squareds[i];
-      nineGrids.sort();
-      
-      for (let j = 0; j < LEN; j++) {
-        if (nineGrids[j] !== NOTREPEATROWS[j]) { return false; }
-      }
+
+    for (let i = 0; i < LEN; i += CELLGROUPCOUNTER) {
+        for (let j = 0; j < LEN; j += CELLGROUPCOUNTER) {
+            let squared = this.inSquareds(i, j, sudoku).sort(),
+                isSquaredTrue = this.checkSudokuRows(squared);
+
+            if (!isSquaredTrue) { return isSquaredTrue; }
+          }
     }
   
     return true;
   }
 
 }
-
-// export default SudokuCore;
